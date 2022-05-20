afd5d381ce2e303b87f7c1669635dfcc8a040734
RashidRysaev <rashid.rysaev93@gmail.com>
2022-05-20 12:17:38 +0200

    Contact page added; parsing functions added

--- core.py
+++ core.py
@@ -24,12 +24,19 @@ class App:
         :param start_response:
         :return:
         """
+        request_method = environment['REQUEST_METHOD']
+        query_string = environment['QUERY_STRING']
         path = environment['PATH_INFO']
         if path.endswith('/') and not path == '/':
             path = path[:-1]
-            print('You`ve got your easter egg, man!')
+        data = get_wsgi_input_data(environment)
+        data = parse_wsgi_input_data(data)
+        request_parameters = parse_input_data(query_string)
         if path in self.urls:
             view = self.urls[path]
+            self.request['req_params'] = request_parameters
+            self.request['data'] = data
+            self.request['method'] = request_method
             for controller in self.front_controllers:
                 controller(self.request)
         else:
@@ -37,3 +44,46 @@ class App:
         resp, body = view(self.request)
         start_response(resp, [('Content-Type', 'text/html')])
         return body
+
+
+def parse_input_data(data):
+    """
+    Receives data from a query string.
+
+    :param data: raw query string
+    :return: query data as dict
+    """
+    result = {}
+    if data:
+        parameters = data.split('&') # i.e. ?12+2=14&18-3=15 -> ['12+2=14', '18-3=15']
+        for item in parameters:
+            key, value = item.split('=')
+            result[key] = value
+    return result # i.e {'12+2': '14', '18-3': '15'}
+
+
+def get_wsgi_input_data(environment):
+    """
+    Retrieves the data from the wsgi.input field of a POST-request.
+
+    :param environment: dictionary with all the data
+    :return: data encoded in bytes
+    """
+    query_content_length = environment.get('CONTENT_LENGTH')
+    content_length = int(query_content_length) if query_content_length else 0
+    data = environment['wsgi.input'].read(content_length) \
+        if content_length > 0 else b''
+    return data
+
+
+def parse_wsgi_input_data(raw_data):
+    """
+    Converts the data from a POST-request to a dictionary.
+
+    :param raw_data: raw input data
+    :return: dictionary with values
+    """
+    result = {}
+    if raw_data:
+        result = parse_input_data(raw_data.decode('utf-8'))
+    return result
--- views.py
+++ views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from template_renderer import render_template
 
 
@@ -28,6 +30,42 @@ class AboutPageView:
         """
         return '200 Ok', [render_template(
             'templates/about.html').encode('utf-8')]
+
+
+class ContactPageView:
+    """
+    Simple view rendering the contact form page.
+    """
+
+    def __call__(self, request):
+        """
+        Main callable method that does the magic.
+
+        :param request: HTTP-request
+        :return: tuple, first element is string, second - HTML-code
+        """
+        if request['method'] == 'POST':
+            self.save_to_file(request)
+            return '200 Ok', [render_template(
+                'templates/contact.html').encode('utf-8')]
+        else:
+            return '200 Ok', [render_template(
+                'templates/contact.html').encode('utf-8')]
+
+    
+    def save_to_file(self, request):
+        """
+        Saves data from incoming POST-request to file.
+
+        :param request: incoming data
+        """
+        with open(f"incoming_msg_{datetime.now()}", 'w') as f:
+            text = f"Incoming message:\n\n" \
+                f"From: {request['data']['email']};\n" \
+                f"Subject: {request['data']['subject']};\n" \
+                f"Text:\n{request['data']['message_text']}"
+            f.write(text)
+            f.close()
 
 
 class NotFoundPageView:
